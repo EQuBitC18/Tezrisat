b968a13e8fb2ce9095000c6cb0c6df398e924c9b
EQuBit <equbit18@gmail.com>
2025-07-19 05:30:14 +0200

    Update package.json and api.js for dependency management and API URL configuration

--- Tezrisat_Frontend/tezrisat_frontend/src/api.js
+++ Tezrisat_Frontend/tezrisat_frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/tezrisat/tezrisatbackend/v1"
+const apiUrl = "https://tezrisat-backend.onrender.com"
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
