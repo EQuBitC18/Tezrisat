b694694447f4ce447e124d097ecbfca450276cbf
EQuBit <equbit18@gmail.com>
2025-07-20 03:15:22 +0200

    Add axios dependency and update CORS settings in settings.py

--- Tezrisat_Backend/Tezrisat_Backend/settings.py
+++ Tezrisat_Backend/Tezrisat_Backend/settings.py
@@ -160,6 +160,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://localhost:5174",
     "https://tezrisat-frontend.onrender.com"
 ]
 CORS_ALLOWS_CREDENTIALS = True
