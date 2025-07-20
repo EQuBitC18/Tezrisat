45c636ce89a9cebb390b972a1f9e80f53256c433
EQuBit <equbit18@gmail.com>
2025-07-20 04:30:36 +0200

    Update settings.py to configure database with dj_database_url and modify workspace.xml for task management

--- Tezrisat_Backend/Tezrisat_Backend/settings.py
+++ Tezrisat_Backend/Tezrisat_Backend/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 from pathlib import Path
 from datetime import timedelta
-
 import dj_database_url
 from dotenv import load_dotenv
 import os
@@ -98,20 +97,14 @@ WSGI_APPLICATION = 'Tezrisat_Backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
